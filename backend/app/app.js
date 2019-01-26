71ed770fbe0ebeda2e66f316ceb7f932256a6b4d
mumo-dev <samumo.dev@gmail.com>
2019-01-26 22:33:36 +0300

    created admin dashboard

--- backend/app/app.js
+++ backend/app/app.js
@@ -13,6 +13,7 @@ require('./config/passport')(passport);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -41,6 +42,7 @@ app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/app/routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var adminController = require('../controllers/admin');
+var router = express.Router();
+
+// router.get('/locations/:id', adminController.getLocations);
+
+
+module.exports = router;
