013404621e0c57cc8132ffd951e1bda2ea4c22e8
mumo-dev <samumo.dev@gmail.com>
2019-01-27 21:52:32 +0300

    added restaurants and their menu

--- backend/app/sequelize.js
+++ backend/app/sequelize.js
@@ -1,6 +1,11 @@
 const  Sequelize = require('sequelize');
 
 const UserModel = require('./models/user');
+const TownModel = require('./models/town');
+const AreaModel = require('./models/areas');
+const RestaurantModel = require('./models/restaurant');
+const MenuModel = require('./models/menu');
+
 const config = require('./config/database');
 
 const sequelize = new Sequelize(config.database,config.connection.user, config.connection.password,{
@@ -15,11 +20,28 @@ const sequelize = new Sequelize(config.database,config.connection.user, config.c
 });
 
 const User = UserModel(sequelize,Sequelize);
+const Town = TownModel(sequelize,Sequelize);
+const Area = AreaModel(sequelize,Sequelize);
+const Restaurant = RestaurantModel(sequelize,Sequelize);
+const Menu = MenuModel(sequelize,Sequelize);
+
+Area.belongsTo(Town); //town_id added to area
+Town.hasMany(Area); //town_id added to area
+Restaurant.belongsTo(Town); //town_id added to restaurant
+Town.hasMany(Restaurant);
+Restaurant.hasMany(Menu);
+Menu.belongsTo(Restaurant);
+
+
 sequelize.sync({force:false})
     .then(()=> console.log('Tables created'))
     .catch(err=> console.log(err));
 
 module.exports ={
-    User
+    User,
+    Town,
+    Area,
+    Restaurant,
+    Menu
 };
 
