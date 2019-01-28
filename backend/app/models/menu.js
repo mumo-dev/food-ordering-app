cb0a4868e33f5f99af473cffb1cd461ae9a67e26
mumo-dev <samumo.dev@gmail.com>
2019-01-28 21:47:03 +0300

    display restaurant menus

---
+++ backend/app/models/menu.js
@@ -0,0 +1,15 @@
+module.exports = function (sequelize, DataTypes) {
+
+    return sequelize.define("menus", {
+        id: {
+            type:DataTypes.INTEGER,
+            autoIncrement:true,
+            primaryKey:true
+        },
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        price:DataTypes.INTEGER,
+        imageUrl:DataTypes.STRING
+    });
+
+};
