499dd327c22c633695704154de508025be35f31a
mumo-dev <samumo.dev@gmail.com>
2019-01-29 10:41:42 +0300

    added pagination

--- backend/app/routes/api.js
+++ backend/app/routes/api.js
@@ -6,5 +6,8 @@ router.get('/menus', menuController.findAllMenus);
 router.get('/menus/location/:town_id', menuController.findAllMenusInTown);
 router.get('/menus/:id', menuController.findMenuById);
 
+router.get('/towns', menuController.findTowns);
+router.get('/towns/:id', menuController.findTownById);
+
 
 module.exports = router;
